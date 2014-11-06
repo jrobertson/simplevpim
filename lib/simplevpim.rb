aa13895a5e0caa8bba9af971ed6905f309d057c1
Your Name <you@example.com>
2014-11-06 21:32:26 +0000

    feature: added the following fields: email, photo, telephone, categories, source, twitter, and jabber

--- lib/simplevpim.rb
+++ lib/simplevpim.rb
@@ -19,9 +19,9 @@ class SimpleVpim
     end
 
   end
-
-  private
-
+  
+  alias to_vcf to_vcard
+    
   def make_vcard(h)
 
     prefix = h[:prefix]
@@ -46,6 +46,14 @@ class SimpleVpim
     end
 
     card = Vpim::Vcard::Maker.make2 do |maker|
+      
+      def  maker.add(field_name, value, params={})        
+        
+        field = Vpim::DirectoryInfo::Field.create field_name, value, params
+        add_field field
+        
+      end
+      
       maker.add_name do |name|
         name.prefix = prefix if prefix
         name.given = firstname
@@ -54,11 +62,25 @@ class SimpleVpim
         name.fullname = fullname if fullname
       end
 
+      # -- email -----------------------------
+      
       e = h[:email]
-      eh = h[:email][:home]
-      ew = h[:email][:work]
-      maker.add_email(ew) { |e| e.location = 'work' } if ew
-      maker.add_email(eh) { |e| e.location = 'home' } if eh
+      
+      if e then
+        
+        if e.is_a? String then
+          maker.add_email e
+        else
+          eh = h[:email][:home]
+          ew = h[:email][:work]
+          maker.add_email(ew) { |e| e.location = 'work' } if ew
+          maker.add_email(eh) { |e| e.location = 'home' } if eh
+        end
+      end
+      
+      # -- urls ---------------------------------
+      
+      h[:url] ||= h[:urls]
       
       if h[:url] then
         
@@ -68,11 +90,48 @@ class SimpleVpim
           
           # unfortunately vPim doesn't use a block with the add_url method
           #maker.add_url (h[:url][:work]){|e| e.location = 'work'} if h[:url][:work]
+          
           h[:url][:items].each {|url| maker.add_url url }
           
         end
         
       end
+      
+      # -- photos
+      
+      maker.add_photo {|photo| photo.link = h[:photo] } if h[:photo]
+      
+      # -- telephone
+      
+      tel = h[:tel]
+      
+      if tel then
+      
+        if tel.is_a? String then
+          
+          maker.add_tel tel
+          
+        else
+          th = h[:tel][:home]
+          tw = h[:tel][:work]
+          maker.add_tel(tw) { |e| e.location = 'work' } if tw
+          maker.add_tel(th) { |e| e.location = 'home' } if th
+        end
+      end
+      
+      # -- categories ------------      
+      maker.add 'CATEGORIES', h[:categories].split(/\s*,\s*/) if h[:categories]
+      
+      # -- source ------------      
+      maker.add 'SOURCE', h[:source] if h[:source]
+
+      # -- Twitter ------------      
+      maker.add 'X-TWITTER', h[:twitter] if h[:twitter]
+      
+      # -- XMPP
+      xmpp = h[:jabber] || h[:xmpp]
+      maker.add 'X-JABBER', xmpp, 'TYPE' => 'im' if xmpp
+      
     end
   end
 
